769313f8237e35efea219e70da6a1b3000187b4e
Marco Nembrini <marco@3ap.ch>
2021-07-09 14:47:47 +0200

    Initial commit

---
+++ src/main/java/ch/aaap/engineering/example/connectionpooldebug/ConnectionPoolDebugApplication.java
@@ -0,0 +1,13 @@
+package ch.aaap.engineering.example.connectionpooldebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionPoolDebugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectionPoolDebugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/aaap/engineering/example/connectionpooldebug/ConnectionPoolDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.aaap.engineering.example.connectionpooldebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectionPoolDebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
